55651bdf8c05fc15a7e32ff74d1007376e5e650d
Otis <altispec@gmail.com>
2015-11-14 21:39:41 +0800

    [9.2.5] Finish injecting dependencies as roles

--- testing_incoming/gear_test.rb
+++ testing_incoming/gear_test.rb
@@ -1,14 +1,20 @@
 require_relative "../minitest_helper"
 require_relative "Gear"
 
+class DiameterizableDouble
+  def diameter
+    10
+  end
+end
+
 class GearTest < Minitest::Test
   def test_calculates_gear_inches
     gear = Gear.new(
-      chainring: 52,
+      chainring: 44,
       cog: 11,
-      wheel: Wheel.new(26, 1.5)
+      wheel: DiameterizableDouble.new
     )
 
-    assert_in_delta 137.1, gear.gear_inches, 0.01
+    assert_in_delta 40, gear.gear_inches, 0.01
   end
 end

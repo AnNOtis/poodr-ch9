6fc511bc4f0c952eac95c2e11bce23c776fd75d7
Otis <altispec@gmail.com>
2015-11-14 21:50:39 +0800

    [9.2.5] Implement the diameterizable interface for Wheel

--- testing_incoming/wheel_test.rb
+++ testing_incoming/wheel_test.rb
@@ -2,6 +2,14 @@ require_relative "../minitest_helper"
 require_relative "wheel"
 
 class WheelTest < Minitest::Test
+  def setup
+    @wheel = Wheel.new(26, 1.5)
+  end
+
+  def test_implements_the_diameterizable_interface
+    assert_respond_to @wheel, :diameter
+  end
+
   def test_calculates_diameter
     wheel = Wheel.new(26, 1.5)
 

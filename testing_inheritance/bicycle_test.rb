ec61f6e3fec4e25bb8b59412d26cdadaa936f334
Otis <altispec@gmail.com>
2015-11-16 18:02:30 +0800

    [9.6.3.1] Test puts color in local spares

--- testing_inheritance/bicycle_test.rb
+++ testing_inheritance/bicycle_test.rb
@@ -20,6 +20,10 @@ class RoadBicycleTest < Minitest::Test
   include BicycleSubclassTest
 
   def setup
-    @road_bicycle = @object = RoadBicycle.new
+    @road_bicycle = @object = RoadBicycle.new(tape_color: "red")
+  end
+
+  def test_puts_color_in_local_spares
+    assert_equal "red", @road_bicycle.spares[:tape_color]
   end
 end

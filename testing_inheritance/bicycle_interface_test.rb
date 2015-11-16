114eb151de865a33fbd8f0654bea30df58aee5ae
Otis <altispec@gmail.com>
2015-11-16 17:44:20 +0800

    [9.6.2] Specifying subclass responsibilities

--- testing_inheritance/bicycle_interface_test.rb
+++ testing_inheritance/bicycle_interface_test.rb
@@ -15,18 +15,6 @@ module BicycleInterfaceTest
     assert_respond_to @object, :spares
   end
 
-  def test_responds_to_default_tire_size
-    assert_respond_to @object, :default_tire_size
-  end
-
-  def test_responds_to_post_initialize
-    assert_respond_to @object, :post_initialize
-  end
-
-  def test_responds_to_local_spares
-    assert_respond_to @object, :local_spares
-  end
-
   def test_responds_to_default_chain
     assert_respond_to @object, :default_chain
   end

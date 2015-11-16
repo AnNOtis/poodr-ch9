8f980f7286cc78e4bad392e18c28a80ad344d79e
Otis <altispec@gmail.com>
2015-11-16 14:23:58 +0800

    Refactor to duck typing

--- testing_duck_typing/trip.rb
+++ testing_duck_typing/trip.rb
@@ -1,15 +1,30 @@
 class Mechanic
   def prepare_bicycle(bicycle); end
+
+  def prepare_trip(trip)
+    trip.bicycles.each{ |bicycle| prepare_bicycle(bicycle) }
+  end
 end
 
 class TripCoordinator
   def buy_food(customers); end
+
+  def prepare_trip(trip)
+    buy_food(trip.customers)
+  end
 end
 
 class Driver
   def gas_up(vehicle); end
 
   def fill_water_tank(vehicle); end
+
+  def prepare_trip(trip)
+    vehicle = trip.vehicle
+
+    gas_up(vehicle)
+    fill_water_tank(vehicle)
+  end
 end
 
 class Trip
@@ -17,15 +32,7 @@ class Trip
 
   def prepare(preparers)
     prepares.each do |preparer|
-      case preparer
-      when Mechanic
-        preparer.prepare_bicycles(bicycles)
-      when TripCoordinator
-        preparer.buy_food(customers)
-      when Driver
-        preparer.gas_up(vehicle)
-        preparer.fill_water_tank(vehicle)
-      end
+      preparer.prepare_trip(self)
     end
   end
 end
